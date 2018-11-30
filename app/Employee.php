9ac042f321db7d6a548cbb1620b44d7a9c55366d
Eugene Krysanov <eugene.krysanov@gmail.com>
2018-11-30 20:56:08 +0500

    Admin page  - added inverse relationships to Employee model

--- app/Employee.php
+++ app/Employee.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     public $timestamps = false;
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
+    public function shift()
+    {
+        return $this->belongsTo('App\Shift');
+    }
+
+    public function rank()
+    {
+        return $this->belongsTo('App\Rank');
+    }
 }
