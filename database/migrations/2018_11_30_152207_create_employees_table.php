abca533c3054f281ca55c0b1b95bff6f8cfd0d5d
Eugene Krysanov <eugene.krysanov@gmail.com>
2018-11-30 20:51:42 +0500

    Admin page  - added Employee model (new files)

---
+++ database/migrations/2018_11_30_152207_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('lastname', 20);
+            $table->char('firstname', 20);
+            $table->char('middlename', 20);
+            $table->integer('shift_id');
+            $table->integer('post_id');
+            $table->integer('rank_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
