d0567c974e4c5d09cc4576bda91ca522bfcfe93b
Eugene Krysanov <eugene.krysanov@gmail.com>
2019-05-27 14:06:06 +0500

    - add QCod\ImageUp

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Kris\LaravelFormBuilder\FormBuilderServiceProvider::class,
+        QCod\ImageUp\ImageUpServiceProvider::class,
     ],
 
     /*
