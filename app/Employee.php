6aa11bbe44412d3f4055db26f95d1414abfc5c69
Eugene Krysanov <eugene.krysanov@gmail.com>
2019-05-28 15:08:29 +0500

    - refactor admin controller and employee model

--- app/Employee.php
+++ app/Employee.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Employee extends Model
 {
@@ -21,5 +22,38 @@ class Employee extends Model
     public function rank()
     {
         return $this->belongsTo('App\Rank');
+    }
+
+    public static function saveData(Request $request, $id = null)
+    {
+        if (is_null($id)) {
+            $employee = new Employee();
+        } else {
+            $employee = self::find($id);
+        }
+
+
+        $employee->firstname = $request->firstname;
+        $employee->lastname = $request->lastname;
+        $employee->middlename = $request->middlename;
+        $employee->post_id = $request->post;
+        $employee->rank_id = $request->rank;
+        $employee->shift_id = $request->shift;
+
+        if ($request->croppedImage) {
+            $image_name = $employee->lastname."_".time().".png";
+            $path = public_path()."/upload/employees/".$image_name;
+
+            list(, $image_src) = explode(",", $request->croppedImage);
+
+            if (file_put_contents($path, base64_decode($image_src))) {
+                $employee->pic_path = $image_name;
+            }
+        }
+
+        if ($employee->save()) {
+            $request->session()->flash('message',
+                "Добавлен новый сотрудник - {$employee->lastname} {$employee->firstname}");
+        }
     }
 }
--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,6 +31,12 @@ class AdminController extends Controller
         return view('admin.index', compact('employees'));
     }
 
+    /**
+     * Get page for creating a new employee
+     *
+     * @param FormBuilder $formBuilder
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function newEmployee(FormBuilder $formBuilder)
     {
         $form = $formBuilder->create('App\Forms\AddEmployeeForm', [
@@ -40,6 +46,13 @@ class AdminController extends Controller
         return view('admin.employee', compact('form'));
     }
 
+    /**
+     * Get page for editing employee's data
+     *
+     * @param $id
+     * @param FormBuilder $formBuilder
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getEmployee($id, FormBuilder $formBuilder)
     {
         $employee = Employee::find($id)->toArray();
@@ -49,39 +62,17 @@ class AdminController extends Controller
 
     public function addEmployee(Request $request)
     {
-        $employee = new Employee();
+        Employee::saveData($request);
 
-        $employee->firstname = $request->firstname;
-        $employee->lastname = $request->lastname;
-        $employee->middlename = $request->middlename;
-        $employee->post_id = $request->post;
-        $employee->rank_id = $request->rank;
-        $employee->shift_id = $request->shift;
-
-        if ($request->croppedImage) {
-            $image_name = $employee->lastname."_".time().".png";
-            $path = public_path()."/upload/employees/".$image_name;
-
-            list(, $image_src) = explode(",", $request->croppedImage);
-
-            if (file_put_contents($path, base64_decode($image_src))) {
-                $employee->pic_path = $image_name;
-            }
-        }
-
-        if ($employee->save()) {
-            $request->session()->flash('message',
-                "Добавлен новый сотрудник - {$employee->lastname} {$employee->firstname}");
-        }
         return redirect()->route('admin');
     }
 
-    public function removeEmployee($id)
+    public function updateEmployee($id)
     {
 
     }
 
-    public function updateEmployee($id)
+    public function removeEmployee($id)
     {
 
     }
