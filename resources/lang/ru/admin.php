65888f6534606eadc1f58080458b6620ec8da968
Eugene Krysanov <eugene.krysanov@gmail.com>
2019-05-29 12:16:43 +0500

    - add some lang files

---
+++ resources/lang/ru/admin.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'mainPage' => '#',
+    'logout' => '#',
+    'login' => '#',
+    'register' => '#',
+    'id' => '#',
+    'name' => 'ФИО',
+    'post' => '',
+    'rank' => '',
+    'shift' => '',
+    'delete' => '',
+    'save' => '',
+    'edit' => '',
+    'addEmployee' => '',
+    'choosePhoto' => '',
+    'list' => '',
+];
---
+++ resources/lang/ru/employee.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'firstname' => '',
+    'lastname' => '',
+    'middlename' => '',
+    'rank' => '',
+    'post' => '',
+    'shift' => '',
+    'photo' => '',
+    'choosePhoto' => '',
+    'crop' => '',
+    'save' => '',
+];
