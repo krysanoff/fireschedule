974aef80f2458d56db06b784ebf1c00f7511db35
Eugene Krysanov <eugene.krysanov@gmail.com>
2019-06-01 14:17:47 +0500

    - design index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <title>{{ config('app.name') }} - График дежурства</title>
 
@@ -13,10 +13,35 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
     </head>
-    <body>
-    <a href="{{ route('graph', ['shift' => 1]) }}" class="btn">1 караул</a>
-    <a href="{{ route('graph', ['shift' => 2]) }}" class="btn">2 караул</a>
-    <a href="{{ route('graph', ['shift' => 3]) }}" class="btn">3 караул</a>
-    <a href="{{ route('graph', ['shift' => 4]) }}" class="btn">4 караул</a>
+    <body class="body-backgroound">
+        <div class="container">
+            <ul class="list-unstyled">
+                <li class="row mt-5">
+                    <a href="{{ route('graph', ['shift' => 1]) }}" class="btn btn-lg btn-light text-dark text-uppercase">
+                        1 караул
+                    </a>
+                </li>
+                <li class="row mt-5">
+                    <a href="{{ route('graph', ['shift' => 2]) }}" class="btn btn-lg btn-light text-dark text-uppercase">
+                        2 караул
+                    </a>
+                </li>
+                <li class="row mt-5">
+                    <a href="{{ route('graph', ['shift' => 3]) }}" class="btn btn-lg btn-light text-dark text-uppercase">
+                        3 караул
+                    </a>
+                </li>
+                <li class="row mt-5">
+                    <a href="{{ route('graph', ['shift' => 4]) }}" class="btn btn-lg btn-light text-dark text-uppercase">
+                        4 караул
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+
+
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
