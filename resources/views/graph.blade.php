79460e83ba0c78b69dd25341b3e4136693e14bb1
Eugene Krysanov <eugene.krysanov@gmail.com>
2020-02-06 07:13:09 +0300

    refactor blade templates

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -1,20 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.app')
 
-        <script src="/js/lang.js"></script>
-
-        <title>{{ config('app.name') }} - График дежурства</title>
-
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-    </head>
-    <body>
+@section('content')
         <header class="navbar navbar-expand bg-dark text-light shadow d-print-none mb-5">
             <div class="container-fluid">
                 <div class="col-3">
@@ -41,6 +27,11 @@
                 {{ __('messages.graph.error') }}
             </p>
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+        <script src="{{ mix('js/app.js') }}"></script>
+@endsection
+
+@section('footer')
+    <footer>
+        <p>Сервис разработан <a href="mailto:eugene.krysanov@gmail.com">Евгением Крысановым</a> &#169; 2019 - {{ date('Y') }}</p>
+    </footer>
+@endsection
