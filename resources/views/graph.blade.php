c1570e51917293a00d9697e69a95bcc3b4b2763b
Eugene Krysanov <eugene.krysanov@gmail.com>
2020-02-07 07:55:31 +0300

    add footer on graph page

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -31,7 +31,20 @@
 @endsection
 
 @section('footer')
-    <footer>
-        <p>Сервис разработан <a href="mailto:eugene.krysanov@gmail.com">Евгением Крысановым</a> &#169; 2019 - {{ date('Y') }}</p>
+    <footer class="footer bg-dark text-center">
+        <div>
+            <p class="footer__text">
+                Сервис разработан
+                <a class="footer__text footer__link" href="https://www.facebook.com/eugene.krysanov" target="_blank">
+                    Евгением Крысановым
+                </a> &#169; 2019 - {{ date('Y') }}
+            </p>
+            <p class="footer__text">
+                С предложениями и пожеланиями можете обращаться по адресу
+                <a class="footer__text footer__link" href="mailto:eugene.krysanov@gmail.com" target="_blank">
+                    eugene.krysanov@gmail.com
+                </a>
+            </p>
+        </div>
     </footer>
 @endsection
