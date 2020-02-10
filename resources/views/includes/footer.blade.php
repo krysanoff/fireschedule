a0fe8d1fd40cb312a5df5d2f864bb8ea8f229f7d
Eugene Krysanov <eugene.krysanov@gmail.com>
2020-02-10 05:45:30 +0300

    update footer styles

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,10 +4,10 @@
             Сервис разработан
             <a class="footer__text footer__link" href="https://www.facebook.com/eugene.krysanov" target="_blank">
                 Евгением Крысановым
-            </a> &#169; 2019 - {{ date('Y') }}
+            </a> 2019 - {{ date('Y') }} гг.
         </p>
         <p class="footer__text">
-            С предложениями и пожеланиями можете обращаться по адресу
+            С предложениями и пожеланиями обращайтесь по адресу
             <a class="footer__text footer__link" href="mailto:eugene.krysanov@gmail.com" target="_blank">
                 eugene.krysanov@gmail.com
             </a>
