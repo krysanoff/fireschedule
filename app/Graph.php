c1b77fd279491c72f2a5cddc7eb729dfe09bb5f0
Eugene Krysanov <eugene.krysanov@gmail.com>
2020-02-16 08:24:20 +0300

    Add user id for getting last graph

--- app/Graph.php
+++ app/Graph.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Graph extends Model
 {
@@ -14,9 +15,15 @@ class Graph extends Model
         return $this->belongsTo('App\Shift');
     }
 
-    public static function getLast($shift_id)
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public static function getLast($user_id, $shift_id)
     {
         $lastGraph = self::orderBy('created_at', 'desc')
+                        ->where('user_id', $user_id)
                         ->where('shift_id', $shift_id)
                         ->first();
 
@@ -45,14 +52,16 @@ class Graph extends Model
 
     public static function saveGraph(Request $request)
     {
+        $user = Auth::user();
         $post = $request->all();
         $graph = new Graph();
 
         $graph->created_at = new \DateTime();
         $graph->updated_at = null;
-        $graph->list = json_encode($post['list']);
-        $graph->shift_id = $post['shift_id'];
-        $graph->schema = ""; //TODO change it when there will 2 or more schemas
+        $graph->list       = json_encode($post['list']);
+        $graph->shift_id   = $post['shift_id'];
+        $graph->schema     = ""; //TODO change it when there will 2 or more schemas
+        $graph->user_id    = $user->id;
 
         if ($graph->save()) {
             return json_encode(['completed' => true]);
--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Graph;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
 
 class GraphController extends Controller
 {
     public function getLastGraph($shift_id) {
-        $lastGraph = Graph::getLast($shift_id);
+        $user = Auth::user();
+        $lastGraph = Graph::getLast($user->id, $shift_id);
 
         return view('graph', compact(['lastGraph', 'shift_id']));
     }
